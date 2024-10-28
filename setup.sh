5e848474510562d3e465f50b82cc52748eb0bb6e
Quang Huy <103049818+u400822@users.noreply.github.com>
2024-10-28 08:22:10 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ yes | pkg update
 yes | pkg upgrade
 pkg i python -y
 pip install requests asyncio aiohttp psutil prettytable
-curl -Lis "https://raw.githubusercontent.com/u400822/setup-termux/refs/heads/main/Rejoin.py" -o /sdcard/download/Rejoin.py
+curl -Ls "https://raw.githubusercontent.com/u400822/setup-termux/refs/heads/main/Rejoin.py" -o /sdcard/download/Rejoin.py
