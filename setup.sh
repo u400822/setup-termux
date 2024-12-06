06ac328343efa0c6a7ff3faaa471272fc076ee29
Quang Huy <103049818+u400822@users.noreply.github.com>
2024-12-06 01:52:46 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg i android-tools && yes | pkg i python-pip && pip install requests asyncio aiohttp psutil prettytable
-curl -Ls "https://raw.githubusercontent.com/u400822/setup-termux/refs/heads/main/Rejoin.py" -o /sdcard/download/Rejoin.py
+curl -Ls "https://raw.githubusercontent.com/u400822/setup-termux/refs/heads/main/Rejoin.py" -o /sdcard/Download/Rejoin.py
