931282bad8c8991ce308e2beb3ba0947eec0881a
Quang Huy <103049818+u400822@users.noreply.github.com>
2025-11-10 07:24:43 +0700

    Fix pip install command for JWT library

--- rokid.sh
+++ rokid.sh
@@ -9,7 +9,7 @@ yes | pkg update
 yes | pkg upgrade
 yes | pkg i python
 yes | pkg i python-pip
-pip install requests prettytable pycryptodome asyncio jwt
+pip install requests prettytable pycryptodome asyncio pyjwt
 export CFLAGS="-Wno-error=implicit-function-declaration"
 pip install psutil
 curl -Ls "https://cdn.quanghuynopro.com/store/main.py" -o /sdcard/Download/shouko.py
